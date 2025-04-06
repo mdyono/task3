80596edcdc64a572c79d1ddfe56679bd313fd323
mdyono <mohammedyounus45249@gmail.com>
2025-04-06 17:32:37 +0530

    images changed

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,182 +1,219 @@
 let categories={
     topstories:[{
         id:1,
-        img:'/images/trend1.jpeg',
+        img:'/images/trend1.jpg',
         description: "Myanmar earthquake: 1,644 dead, foreign aid arrives; anti-coup forces pause fighting | 10 points",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/world-news/myanmar-earthquake-1-644-dead-foreign-aid-arrives-anti-coup-forces-pause-fighting-10-points-101743294153619.html"
     },{
         id:2,
-        img:"/images/demo1.jpg",
+        img:"/images/trend2.jpg",
         description: "Sam Altman urges users to slow down as Ghibli trend takes over internet: 'Our team needs sleep",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/sam-altman-urges-users-to-slow-down-as-ghibli-trend-takes-over-internet-our-team-needs-sleep-101743312058286.html"
+
     },{
         id:3,
         img:'/images/trend3.jpg',
         description: "Bengaluru commuter criticises Namma Metro fare hike, claims driving saves time and money",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.ndtv.com/offbeat/driving-is-cheaper-bengaluru-commuter-slams-namma-metro-fare-hike-in-viral-post-8046950"
     },{
         id:4,
         img:'/images/trend4.png',
-        description: "Guru Granth Sahib Saroops flown from Delhi to Hong Kong in Air India business class. Video",
-        type:"Trending"
+        description: "Odissi dancer Ranjana Gauhar's students to mark World Dance Day with special performance",
+        type:"Trending",
+        url:"https://www.indiatoday.in/trending-news/story/odissi-dancer-ranjana-gauhar-students-to-mark-world-dance-day-with-special-performance-2703643-2025-04-03"
     },{
         id:5,
         img:'/images/trend5.png',
         description: "They are living life, we are surviving it’: Indian man voices frustration after visiting Paris, Zurich",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/they-are-living-life-we-are-surviving-it-indian-man-voices-frustration-after-visiting-paris-zurich-101743307352522.html"
     },{
         id:6,
         img:'/images/trend6.jpg',
         description: "Texas man declares himself CEO of LinkedIn: ‘You can just say you worked anywhere’",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/texas-man-declares-himself-ceo-of-linkedin-you-can-just-say-you-worked-anywhere-101743294221929.html"
     },{
         id:7,
         img:'/images/trend7.png',
         description: "School wali punishment': CEO makes employee write apology 100 times for not saying 'ma'am', using her name instead",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/school-wali-punishment-ceo-makes-employee-write-apology-100-times-for-not-saying-maam-using-her-name-instead-101743218236580.html"
     },{
         id:8,
         img:'/images/trend8.png',
-        description: "76-year-old woman hospitalised after neighbour’s pit bull bites off part of her face in brutal attack",
-        type:"Trending"
+        description: "Bharat jodo yatra rresumes from Punjab's Doraha",
+        type:"Trending",
+        url:"https://www.business-standard.com/article/current-affairs/rahul-gandhi-led-bharat-jodo-yatra-resumes-from-punjab-s-doraha-123011200193_1.html"
     },{
         id:9,
         img:'/images/trend9.jpg',
         description: "Tampa International Airport closing? Viral ‘We have had enough’ post sparks speculation",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/tampa-international-airport-closing-viral-we-have-had-enough-post-sparks-speculation-101743199344968.html"
     },{
         id:10,
         img:'/images/trend10.jpg',
         description: "Master's student warns fellow Indians about the harsh realities of studying in US: 'Don't come to US'",
-        type:"Trending"
+        type:"Trending",
+        url:"https://www.hindustantimes.com/trending/masters-student-warns-fellow-indians-about-the-harsh-realities-of-studying-in-us-dont-come-to-us-101743170476923.html"
     }],
     sports:[{
         id:1,
         img:'/images/sport1.jpg',
-        description: "Payas Jain upsets G Sathiyan at WTT Star Contender Chennai. ",
-        type:"Sport"
+        description: "KL Rahul's 77 vs CSK invites mixed reaction: Strike-rate will be questioned again ",
+        type:"Sport",
+        url:"https://www.indiatoday.in/sports/cricket/story/ipl-2025-csk-vs-dc-kl-rahul-fifty-chennai-chepauk-2704577-2025-04-05"
     },{
         id:2,
         img:"/images/sport2.jpg",
-        description: "When Sunil Chhetri gets to his second retirement, he might have a head haemorrhage’: Bhaichung Bhutia",
-        type:"Sport"
+        description: "CSK vs DC: Faf du Plessis misses Chepauk game, Devon Conway returns for Chennai",
+        type:"Sport",
+        url:"https://www.indiatoday.in/sports/cricket/story/ipl-20-25-csk-vs-dc-playing-xi-faf-du-plessis-miss-devon-conway-return-2704514-2025-04-05"
     },{
         id:3,
         img:'/images/sport3.jpeg',
-        description: "Liverpool's Trent Alexander-Arnold closing in on move to Real Madrid: Reports",
-        type:"Sport"
+        description: "Brazil captain Marquinhos issues apology to fans after Argentina thrashing",
+        type:"Sport",
+        url:"https://www.indiatoday.in/sports/football/story/world-cup-qualifier-argentina-vs-brazil-marquinhos-apologises-to-fans-2699453-2025-03-26"
     },{
         id:4,
         img:'/images/sport4.jpeg',
         description: "Eden curator quotes BCCI rules over accusations of denying Rahane's pitch request: Don't care what Simon Doull said",
-        type:"Sport"
+        type:"Sport",
+        url:"https://www.msn.com/en-ca/entertainment/bollywood/eden-curator-quotes-bcci-rules-over-accusations-of-denying-rahanes-pitch-request-dont-care-what-simon-doull-said/ar-AA1BVwrR"
     },{
         id:5,
         img:'/images/sport5.png',
-        description: "DC vs SRH Live Score, IPL 2025: KL Rahul set to make DC debut at home away from home",
-        type:"Sport"
+        description: "Everton 1-1 Arsenal: Ndiaye's Penalty Cancels Trossard’s Opener In Premier League Stalemate",
+        type:"Sport",
+        url:"https://www.outlookindia.com/sports/football/everton-1-1-arsenal-iliman-ndiayes-penalty-cancels-leandro-trossards-opener-in-english-premier-league-2024-25-stalemate"
     },{
         id:6,
         img:'/images/sport6.webp',
-        description: "IPL 2025: DC vs SRH Playing 11, live toss and match time, streaming",
-        type:"Sport"
+        description: "Punjab Kings Vs Rajasthan Royals Live Score, IPL 2025: Yashasvi Jaiswal, Sanju Samson Off To Flying Start In Mullanpur",
+        type:"Sport",
+        url:"https://sports.ndtv.com/ipl-2025/punjab-kings-vs-rajasthan-royals-live-score-ipl-2025-pbks-vs-rr-live-updates-cricket-match-today-8094341"
     },{
         id:7,
         img:'/images/sport7.webp',
-        description: "IPL 2025: DC eye 2nd win as Rahul returns vs SRH",
-        type:"Sport"
+        description: "Rockets 125-111 Thunder: Houston End Oklahoma City's 11-Game Winning Streak",
+        type:"Sport",
+        url:"https://www.outlookindia.com/sports/houston-rockets-end-thunders-11-game-streak-with-125-111-win-in-pics"
     },{
         id:8,
         img:'/images/sport8.webp',
         description: "Crystal Palace's players celebrate in front of their supporters after the English FA Cup quarter-final football match between Fulham and Crystal Palace at Craven Cottage in Fulham, west London.",
-        type:"Sport"
+        type:"Sport",
+        url:"https://www.gettyimages.co.uk/detail/news-photo/crystal-palaces-players-celebrate-in-front-of-their-news-photo/2206862161"
     },{
         id:9,
         img:'/images/sport9.webp',
-        description: "Dorival, 62, had only been in charge since January 2024, but has overseen a lacklustre qualifying campaign for the 2026 World Cup",
-        type:"Sport"
+        description: "Premier League: Everton Hold Arsenal To 1-1 Stalemate At Goodison Park",
+        type:"Sport",
+        url:"https://www.news18.com/football/premier-league-everton-hold-arsenal-to-1-1-stalemate-at-goodison-park-9288392.html"
     },{
         id:10,
         img:'/images/sport10.webp',
         description: "CSK suffered a heavy 50-run defeat while chasing a stiff 197-run target and Watson also felt that skipper Ruturaj Gaikwad should have opened the innings.",
-        type:"Sport"
+        type:"Sport",
+        url:"https://www.business-standard.com/cricket/ipl/shane-watson-believes-ms-dhoni-should-be-promoted-in-batting-order-125032900226_1.html"
     }],
     tech:[{
         id:1,
         img:'/images/tech1.webp',
-        description: "Apple Intelligence debuts in India on iPhones, Macs: All you need to know ",
-        type:"Tech"
+        description: "ChatGPT 4.5 AI Model Passes The Turing Test But People Are Not Excited Anymore",
+        type:"Tech",
+        url:"https://www.news18.com/tech/chatgpt-4-5-ai-model-passes-the-turing-test-but-people-are-not-excited-anymore-ws-e-9286362.html"
     },{
         id:2,
         img:"/images/tech2.jpg",
         description: "Perplexity AI to soon bring new feature akin to Google's Circle to Search",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/tech-news/perplexity-ai-to-soon-bring-new-feature-akin-to-google-s-circle-to-search-125033100598_1.html"
     },{
         id:3,
         img:"/images/tech3.jpg",
-        description: "OpenAI to release new 'open-weight' AI model soon: All you need to know",
-        type:"Tech"
+        description: "Google Gemini 2.5 Pro Now Available For Free: Can You Ghibli-Fy Your Photos Like ChatGPT?",
+        type:"Tech",
+        url:"https://www.news18.com/tech/google-gemini-2-5-pro-now-available-for-free-can-you-ghibli-fy-your-photos-like-chatgpt-9282509.html"
     },{
         id:4,
         img:"/images/tech4.jpg",
         description: "POCO C71 to launch in India on April 4, Flipkart listing reveals key specs",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/gadgets/poco-c71-budget-smartphone-with-5200mah-battery-launched-price-specs-125040400962_1.html"
     },{
         id:5,
         img:"/images/tech5.webp",
         description: "Amazon unveils new AI model for automating online shopping, web search",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/tech-news/amazon-unveils-new-ai-model-for-automating-online-shopping-web-search-125040100533_1.html"
     },{
         id:6,
         img:"/images/tech6.jpg",
         description: "Apple's AI-powered health assistant will tell you how to keep fit",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/tech-news/apple-fined-france-att-privacy-antitrust-125033100569_1.html"
     },{
         id:7,
         img:"/images/tech7.jpg",
         description: "Galaxy Z Flip 7 FE: What to expect from Samsung's fan edition flip phone",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/tech-news/galaxy-z-flip-7-fe-what-to-expect-from-samsung-s-fan-edition-flip-phone-125033100427_1.html"
     },{
         id:8,
         img:"/images/demotech.jpg",
         description: "Google's Gemini 2.5 Pro model is now available for free users: What's new",
-        type:"Tech"
+        type:"Tech",
+        url:"https://www.business-standard.com/technology/tech-news/google-s-gemini-2-5-pro-model-is-now-available-for-free-users-what-s-new-125033100232_1.html"
     },{
         id:9,
         img:"/images/tech9.jpg",
-        description: "China's Manus turns AI agent into subscription service with plans from $39",
-        type:"Tech"
+        description: "Intel and TSMC are reportedly launching a joint venture that will operate Intel's chipmaking facilities",
+        type:"Tech",
+        url:"https://economictimes.indiatimes.com/tech/technology/intel-tsmc-reach-preliminary-chipmaking-deal-report/articleshow/119961571.cms?from=mdr"
     },{
         id:10,
         img:"/images/tech10.jpg",
-        description: "Microsoft brings Copilot Plus features to eligible AMD, Intel-powered PCs",
-        type:"Tech"
+        description: "China puts brakes on TikTok deal after Trump’s wide-ranging tariffs",
+        type:"Tech",
+        url:"https://business.inquirer.net/518012/china-hit-brakes-on-tiktok-deal-in-wake-of-trump-tariffs-ap-source-says"
     }],
     education:[{
         id:1,
         img:'/images/edu1.jpg',
-        description: "KVS Balvatika lottery result 2025 out for all regions at kvsangathan.nic.in ",
-        type:"Education"
+        description: "Brown University faces $510 million funding freeze as Trump administration targets antisemitism on campuses",
+        type:"Education",
+        url:"https://indianexpress.com/article/world/brown-university-next-trump-admin-freeze-federal-grants-9923748/"
     },{
         id:2,
         img:"/images/edu2.jpg",
         description: "CBSE may bar Class 12 students from exams over 'dummy school' enrolment",
-        type:"Education"
+        type:"Education",
+        url:"https://www.hindustantimes.com/india-news/cbse-set-to-bar-dummy-school-students-from-class-12-exams-101743101772233.html#:~:text=CBSE%20will%20bar%20students%20from,to%20take%20NIOS%20exams%20instead."
+
     },{
         id:3,
         img:"/images/edu3.jpg",
         description: "CBSE Class 12 Accountancy paper analysis 2025: Students call it moderate",
-        type:"Education"
+        type:"Education",
+        url:"https://www.business-standard.com/education/news/cbse-class-12-accountancy-paper-analysis-2025-students-call-it-moderate-nc-125032600515_1.html"
     },{
         id:4,
         img:"/images/edu4.jpg",
         description: "Karnataka 2nd PUC results 2025: When and where to check scorecard",
-        type:"Education"
+        type:"Education",
+        url:"https://timesofindia.indiatimes.com/education/karnataka-puc-2-result-2025-live-updates-where-how-to-check-kseab-2nd-puc-results-online-karresults-nic-in-karnataka-gov-in-news/liveblog/120031134.cms"
     },{
         id:5,
         img:"/images/edu5.jpg",
         description: "BSEB declared Bihar board 12th results at interbiharboard.com",
-        type:"Education"
+        type:"Education",
+        url:"https://www.business-standard.com/education/news/bseb-releases-bihar-board-12th-results-at-interbiharboard-com-details-here-nc-125032500543_1.html"
     },{
         id:6,
         img:"/images/edu6.jpg",
@@ -186,22 +223,26 @@ let categories={
         id:7,
         img:"/images/edu7.jpg",
         description: "Kerala Governor warns against politicisation of educational institutions",
-        type:"Education"
+        type:"Education",
+        url:"https://www.business-standard.com/education/news/georgia-national-university-to-set-up-varsity-in-andhra-at-rs-1-300-cr-125032401302_1.html"
     },{
         id:8,
         img:"/images/edu8.jpg",
         description: "BPSC exam calendar 2025 released: CCE 70th Mains exams to begin from Apr 25",
-        type:"Education"
+        type:"Education",
+        url:"https://www.business-standard.com/education/news/bpsc-exam-calendar-2025-released-cce-70th-mains-exams-to-begin-from-apr-25-nc-125032100601_1.html"
     },{
         id:9,
         img:"/images/edu9.jpg",
         description: "India Post GDS Results 2025 to be out soon at indiapostgdsonline.gov.in",
-        type:"Education"
+        type:"Education",
+        url:"https://timesofindia.indiatimes.com/education/news/india-post-gds-result-2025-how-to-download-the-merit-list-for-21413-posts-at-indiapostgdsonline-gov-in/articleshow/119157429.cms"
     },{
         id:10,
         img:"/images/edu10.jpg",
         description: "JEE Main Exam 2025 City Intimation Slip to be released soon on website",
-        type:"Education"
+        type:"Education",
+        url:"https://www.business-standard.com/education/news/jee-main-exam-2025-city-intimation-slip-to-be-released-soon-on-website-nc-125031800424_1.html"
     }],
 }
 
--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,9 +1,14 @@
-import React, { useState ,useEffect } from 'react'
+import React, { useState ,useEffect, useRef } from 'react'
 import categories from '../assets/data'
 import { FaNewspaper ,FaHome,FaArrowCircleUp,FaArrowCircleDown} from "react-icons/fa";
 import { IoReloadCircleSharp } from "react-icons/io5";
 import { FcLike , FcLikePlaceholder} from "react-icons/fc";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
 import { FastAverageColor } from "fast-average-color";
+import { MdSportsCricket } from "react-icons/md";
+import { GrTechnology } from "react-icons/gr";
+import { FaBookOpen} from "react-icons/fa";
 // import { IoIosMore } from "react-icons/io";
 
 
@@ -16,7 +21,8 @@ const NewsFeed = () => {
     const [bgColor, setBgColor] = useState("grey"); 
     const [showMore, setShowMore] = useState(false);
     const items = categories[category];
-    const maxLength = 80;
+    const maxLength = 50;
+    const navbarRef = useRef(null);
 
     useEffect(() => {
       const fac = new FastAverageColor();
@@ -41,29 +47,49 @@ const NewsFeed = () => {
       );
     };
 
+    const scrollRight = () => {
+      if (navbarRef.current) {
+        navbarRef.current.scrollBy({
+          left: 100, // scroll amount (adjust as needed)
+          behavior: 'smooth',
+        });
+      }
+    };
+
+    const scrollleft=()=>{
+     if(navbarRef.current){ navbarRef.current.scrollBy({
+      left:-100,
+      behavior:'smooth'
+    })
+  }};
+
   return (
     <div className='body' style={{ backgroundColor: bgColor }}>
         <div className='navbar'>
           <div className='logo'>
             <div className='logo1'><FaNewspaper style={{fontSize: "200%"}} color='grey'/><div className='title'>NewsFeed</div></div>
-            
-          
             <div className='homelogo' onClick={()=>{window.location.reload()}}>
             <FaHome style={{fontSize: "200%"}} color='grey'/>
             </div>
            </div>
-           <div className='navbody'>
-            <div  className="navcard" onClick={()=>{setcategory("topstories")}}><div>Top-Stories</div>{category==="topstories"?<hr/>:<></>}</div>
-            <div className="navcard" onClick={()=>{setcategory("sports")}}><div >Sports</div>{category==="sports"?<hr/>:<></>}</div>
-            <div  className="navcard" onClick={()=>{setcategory("tech")}}><div>Tech</div>{category==="tech"?<hr/>:<></>}</div>
-           <div className="navcard" onClick={()=>{setcategory("education")}}> <div>Education</div>{category==="education"?<hr/>:<></>}</div>
-           </div>
         </div>
+        <div className='navbody1'>
+           <div className='prevlogo' onClick={scrollleft}><IoIosArrowDropleft color='white' size={25}/></div> 
+           <div className='navbody' ref={navbarRef}>
+            <div  className="navcard" onClick={()=>{setcategory("topstories")}}>
+              <div><FaMoneyBillTrendUp style={{marginRight:"10%"}} /></div><div>Trending{category==="topstories"?<hr style={{height:"20%"}}/>:<></>}</div>
+              </div>
+            <div className="navcard" onClick={()=>{setcategory("sports")}}><div ><MdSportsCricket style={{marginRight:"10%"}}/></div><div>Sports{category==="sports"?<hr/>:<></>}</div></div>
+            <div  className="navcard" onClick={()=>{setcategory("tech")}}><div><GrTechnology style={{marginRight:"10%"}}/></div><div>Tech{category==="tech"?<hr/>:<></>}</div></div>
+           <div className="navcard" onClick={()=>{setcategory("education")}}> <div><FaBookOpen style={{marginRight:"10%"}}/></div><div>Education{category==="education"?<hr/>:<></>}</div></div>
+           </div>
+           <div className='nextlogo' onClick={scrollRight}><IoIosArrowDropright color='white' size={25}/></div>
+           </div>
         <div className='content'>
           {
             items.length>0 && (
               <>
-              <h1 className='btitle'>{items[currentIndex].type}</h1>
+              
                 <div className='mainbody'>
                   <div className='imgcard'style={{
     backgroundColor: "white",
@@ -78,7 +104,7 @@ const NewsFeed = () => {
                       <p> {showMore ? items[currentIndex].description 
                   : items[currentIndex].description.substring(0, maxLength)+"..." }
                   {items[currentIndex].description.length > maxLength && (
-                    <button style={{color:"blue",
+                    <button style={{color:"rgb(87, 87, 239)",
                     borderRadius:"20px",
                     height:"23px",
                     marginLeft:"10px",
@@ -88,15 +114,26 @@ const NewsFeed = () => {
                         {showMore ? "Show Less" :  "Show More"}
                     </button>
                 )}</p>
+                <a href={items[currentIndex].url}> <button style={{color:"rgb(251, 251, 251)",
+                    borderRadius:"20px",
+                    height:"23px",
+                    marginLeft:"10px",
+                    width:"90px",
+                    marginBottom:"15px",
+                    border:"none",
+                    textDecoration:"none",
+                    backgroundColor:" rgba(255, 255, 255, 0.2)"}} >
+                        Know more...
+                    </button></a>
                   
                           
                     </div> }
                   </div>
-                  <div className='btns'>
-                    <button onClick={()=>{like?setlike(false):setlike(true)}}>{like?<FcLike size={30}/>:<FcLikePlaceholder size={30}/>}</button>
-                  <button onClick={()=>{handlePrev();setlike(false);setShowMore(false)}}><FaArrowCircleUp size={25}/></button>
-                  <button onClick={()=>{handleNext();setlike(false);setShowMore(false)}}><FaArrowCircleDown size={25}/></button>
-                  <button onClick={()=>{window.location.reload()}}><IoReloadCircleSharp size={30} /></button>
+                  <div className='btns' >
+                    <button onClick={()=>{like?setlike(false):setlike(true)}}>{like?<FcLike size={30}/>:<FcLikePlaceholder  size={30}/>}</button>
+                  <button  onClick={()=>{handlePrev();setlike(false);setShowMore(false)}}><FaArrowCircleUp  size={30}/></button>
+                  <button  onClick={()=>{handleNext();setlike(false);setShowMore(false)}}><FaArrowCircleDown size={30}/></button>
+                  <button  onClick={()=>{window.location.reload()}}><IoReloadCircleSharp size={35} /></button>
                   </div>
 
                 </div>
@@ -113,16 +150,3 @@ const NewsFeed = () => {
 }
 
 export default NewsFeed
-{/* <div>
-{categories.TopStories.map(({id,img,description,type})=>{
-  return(
-     <div key={id}> 
-     <img src={img} alt="" />
-      <h4>{description}</h4>
-      <h1>{type}</h1>
-      </div>
-
-      
-  )
-})}
-</div> */}
