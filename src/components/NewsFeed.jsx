ebc7d1cf80684a1e6ba01e5d00763ebfcbebf34b
mdyono <mohammedyounus45249@gmail.com>
2025-04-17 17:16:36 +0530

    css changed

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,15 +1,15 @@
-import React, { useState ,useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import categories from '../assets/data'
 // import { FaNewspaper ,FaHome,FaArrowCircleUp,FaArrowCircleDown} from "react-icons/fa";
 // import { IoReloadCircleSharp } from "react-icons/io5";
-import { FcLike , FcLikePlaceholder} from "react-icons/fc";
+import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
-import {  FaArrowRotateRight, FaMoneyBillTrendUp } from "react-icons/fa6";
+import { FaArrowRotateRight, FaMoneyBillTrendUp } from "react-icons/fa6";
 import { FastAverageColor } from "fast-average-color";
 import { MdSportsCricket } from "react-icons/md";
 import { GrTechnology } from "react-icons/gr";
-import { FaBookOpen} from "react-icons/fa";
-import { FaArrowUp,FaArrowDown } from "react-icons/fa6";
+import { FaBookOpen } from "react-icons/fa";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa6";
 import { IoMdShare } from "react-icons/io";
 
 
@@ -18,58 +18,59 @@ import { IoMdShare } from "react-icons/io";
 
 import '../CSS/newsfeed.css'
 const NewsFeed = () => {
-    // console.log(categories)
-    let [category,setcategory]=useState("topstories");
-    let [currentIndex, setCurrentIndex] = useState(0);
-    let [like,setlike]=useState(false);
-    const [bgColor, setBgColor] = useState("grey"); 
-    const [showMore, setShowMore] = useState(false);
-    const items = categories[category];
-    const [maxLength, setMaxLength] = useState(100);
-    const imgCardRef = useRef(null);
-    // const maxLength = 80;
-    const navbarRef = useRef(null);
+  // console.log(categories)
+  let [category, setcategory] = useState("topstories");
+  let [currentIndex, setCurrentIndex] = useState(0);
+  let [like, setlike] = useState(false);
+  const [bgColor, setBgColor] = useState("grey");
+  const [showMore, setShowMore] = useState(false);
+  const items = categories[category];
+  const [maxLength, setMaxLength] = useState(100);
+  const imgCardRef = useRef(null);
+  // const maxLength = 80;
+  const navbarRef = useRef(null);
 
-    useEffect(() => {
-      const fac = new FastAverageColor();
-      const img = new Image();
-      img.crossOrigin = "Anonymous";
-      img.src = items[currentIndex].img;
-  
-      img.onload = () => {
-        const color = fac.getColor(img);
-        setBgColor(color.rgb);
-      };
-    }, [currentIndex]);
+  useEffect(() => {
+    const fac = new FastAverageColor();
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.src = items[currentIndex].img;
 
-    const handleNext = () => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);      
+    img.onload = () => {
+      const color = fac.getColor(img);
+      setBgColor(color.rgb);
     };
-    // console.log(items)
+  }, [currentIndex]);
 
-    const handlePrev = () => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? items.length - 1 : prevIndex - 1
-      );
-    };
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+  // console.log(items)
 
-    
-    const scrollRight = () => {
-      if (navbarRef.current) {
-        navbarRef.current.scrollBy({
-          left: 100, // scroll amount (adjust as needed)
-          behavior: 'smooth',
-        });
-      }
-    };
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? items.length - 1 : prevIndex - 1
+    );
+  };
 
-    const scrollleft=()=>{
-     if(navbarRef.current){ 
+
+  const scrollRight = () => {
+    if (navbarRef.current) {
       navbarRef.current.scrollBy({
-      left:-100,
-      behavior:'smooth'
-    })
-  }};
+        left: 100, // scroll amount (adjust as needed)
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollleft = () => {
+    if (navbarRef.current) {
+      navbarRef.current.scrollBy({
+        left: -100,
+        behavior: 'smooth'
+      })
+    }
+  };
   useEffect(() => {
     const updateMaxLength = () => {
       if (imgCardRef.current) {
@@ -93,8 +94,8 @@ const NewsFeed = () => {
   }, []);
 
   return (
-    <div className='body'  style={{ backgroundColor:bgColor }}>
-        {/* <div className='navbar'>
+    <div className='body' style={{ backgroundColor: bgColor }}>
+      {/* <div className='navbar'>
           <div className='logo'>
             <div className='logo1'><FaNewspaper style={{fontSize: "200%"}} color='grey'/><div className='title'>NewsFeed</div></div>
             <div className='homelogo' onClick={()=>{window.location.reload()}}>
@@ -102,102 +103,106 @@ const NewsFeed = () => {
             </div>
            </div>
         </div> */}
-        <div className='navbody1'>
-           <div className='prevlogo' onClick={scrollleft}><IoIosArrowDropleft color='white' size={35}/></div> 
-           <div className='navbody' ref={navbarRef}>
-            <div  className="navcard"  onClick={()=>{setcategory("topstories")}}>
-              <div><FaMoneyBillTrendUp style={{marginRight:"10%"}} /></div><div>Trending{category==="topstories"?<hr style={{height:"20%"}}/>:<></>}</div>
-              </div>
-            <div className="navcard"  onClick={()=>{setcategory("sports")}}><div ><MdSportsCricket style={{marginRight:"10%"}}/></div><div>Sports{category==="sports"?<hr/>:<></>}</div></div>
-            <div  className="navcard"  onClick={()=>{setcategory("tech")}}><div><GrTechnology style={{marginRight:"10%"}}/></div><div>Tech{category==="tech"?<hr/>:<></>}</div></div>
-           <div className="navcard"  onClick={()=>{setcategory("education")}}> <div><FaBookOpen style={{marginRight:"10%"}}/></div><div>Education{category==="education"?<hr/>:<></>}</div></div>
-           </div>
-           <div className='nextlogo' onClick={scrollRight}><IoIosArrowDropright color='white' size={35}/></div>
-           </div>
-        <div className='content' ref={imgCardRef} style={{
-   backgroundColor:bgColor,
-     
-  }}>
-          {
-            items.length>0 && (
-              <>
-              
-                <div className='mainbody'>
-                  <div className='imgcard' ref={imgCardRef} style={{
-    backgroundColor: "white",
-    backgroundImage: `url(${items[currentIndex].img})`,
-    backgroundSize: "cover", 
-    backgroundPosition: "center",
-    objectFit:"fill"
-     
-  }}>
+      <div className='navbody1'>
+        <div className='prevlogo' onClick={scrollleft}><IoIosArrowDropleft color='white' size={35} /></div>
+        <div className='navbody' ref={navbarRef}>
+          <div className="navcard" onClick={() => { setcategory("topstories") }}>
+            <div><FaMoneyBillTrendUp style={{ marginRight: "10%" }} /></div><div>Trending{category === "topstories" ? <hr style={{ height: "20%" }} /> : <></>}</div>
+          </div>
+          <div className="navcard" onClick={() => { setcategory("sports") }}><div ><MdSportsCricket style={{ marginRight: "10%" }} /></div><div>Sports{category === "sports" ? <hr /> : <></>}</div></div>
+          <div className="navcard" onClick={() => { setcategory("tech") }}><div><GrTechnology style={{ marginRight: "10%" }} /></div><div>Tech{category === "tech" ? <hr /> : <></>}</div></div>
+          <div className="navcard" onClick={() => { setcategory("education") }}> <div><FaBookOpen style={{ marginRight: "10%" }} /></div><div>Education{category === "education" ? <hr /> : <></>}</div></div>
+        </div>
+        <div className='nextlogo' onClick={scrollRight}><IoIosArrowDropright color='white' size={35} /></div>
+      </div>
+      <div className='content' ref={imgCardRef} style={{
+        backgroundColor: bgColor,
 
-<div className='btns' >
-                    <button  onClick={()=>{like?setlike(false):setlike(true)}}>{like?<FcLike />:<FcLikePlaceholder  />}</button>
-                  <button  onClick={()=>{handlePrev();setlike(false);setShowMore(false)}}><FaArrowUp color='white' /></button>
-                  <button  onClick={()=>{handleNext();setlike(false);setShowMore(false)}}><FaArrowDown /></button>
-                  <button  onClick={()=>{window.location.reload()}}><FaArrowRotateRight   /></button>
+      }}>
+        {
+          items.length > 0 && (
+            <>
+
+              <div className='mainbody'>
+                <div className='imgcard' ref={imgCardRef} style={{
+                  backgroundColor: "white",
+                  backgroundImage: `url(${items[currentIndex].img})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  objectFit: "fill"
+
+                }}>
+
+                  <div className='btns' >
+                    <button onClick={() => { like ? setlike(false) : setlike(true) }}>{like ? <FcLike /> : <FcLikePlaceholder />}</button>
+                    <button onClick={() => { handlePrev(); setlike(false); setShowMore(false) }}><FaArrowUp color='white' /></button>
+                    <button onClick={() => { handleNext(); setlike(false); setShowMore(false) }}><FaArrowDown /></button>
+                    <button onClick={() => { window.location.reload() }}><FaArrowRotateRight /></button>
                   </div>
-                    {
+                  {
                     <div className='imgdescription'>
-                      <p  style={{cursor:"pointer"}} onClick={() => setShowMore(!showMore)}> {showMore ? items[currentIndex].description 
-                  : items[currentIndex].description.substring(0, maxLength) }
-                  {items[currentIndex].description.length > maxLength && (
-                    <span 
-                    style={{
-                      // color:"rgb(87, 87, 239)",
-                    borderRadius:"20px",
-                    height:"23px",
-                    // marginLeft:"10px",
-                    width:"30px",
-                    fontSize:"20px",
-                    textAlign:"center",
-                    border:"none",
-                    // backgroundColor:" rgba(255, 255, 255, 0.2)"
-                    }}
-                     onClick={() => setShowMore(!showMore)}>
-                        {showMore ? "" :  "..."}
-                    </span>
-                )}</p>
-                <div style={{display:"flex"}}>
-                <a href={items[currentIndex].url}> <button style={{color:"rgb(251, 251, 251)",
-                    borderRadius:"20px",
-                    height:"40px",
-                    marginLeft:"10px",
-                    width:"130px",
-                    marginBottom:"15px",
-                    cursor:"pointer",
-                    border:"none",
-                    fontSize:"18px",
-                    textDecoration:"none",
-                    backgroundColor:" rgba(28, 27, 27, 0.48)"}} >
-                        Know more...
-                    </button></a>
-                    <a href="#"><button style={{color:"rgb(251, 251, 251)",
-                    borderRadius:"20px",
-                    height:"40px",
-                    marginLeft:"10px",
-                    width:"40px",
-                    cursor:"pointer",
-                    fontSize:"18px",
-                    marginBottom:"15px",
-                    border:"none",
-                    textDecoration:"none",
-                    backgroundColor:" rgba(28, 27, 27, 0.48)"}}>
-                    <IoMdShare />
-                      </button></a>
-                </div>
-               
-                          
-                    </div> }
-                    
-                  </div>
-                
+                      <p style={{ cursor: "pointer" }} onClick={() => setShowMore(!showMore)}> {showMore ? items[currentIndex].description
+                        : items[currentIndex].description.substring(0, maxLength)}
+                        {items[currentIndex].description.length > maxLength && (
+                          <span
+                            style={{
+                              // color:"rgb(87, 87, 239)",
+                              borderRadius: "20px",
+                              height: "23px",
+                              // marginLeft:"10px",
+                              width: "30px",
+                              fontSize: "20px",
+                              textAlign: "center",
+                              border: "none",
+                              // backgroundColor:" rgba(255, 255, 255, 0.2)"
+                            }}
+                            onClick={() => setShowMore(!showMore)}>
+                            {showMore ? "" : "..."}
+                          </span>
+                        )}</p>
+                      <div className='description_btns' style={{ display: "flex" }}>
+                        <a href={items[currentIndex].url}> <button style={{
+                          color: "rgb(251, 251, 251)",
+                          borderRadius: "20px",
+                          height: "40px",
+                          marginLeft: "10px",
+                          width: "130px",
+                          marginBottom: "15px",
+                          cursor: "pointer",
+                          border: "none",
+                          fontSize: "18px",
+                          textDecoration: "none",
+                          backgroundColor: " rgba(28, 27, 27, 0.48)"
+                        }} >
+                          Know more...
+                        </button></a>
+                        <a href="#"><button style={{
+                          color: "rgb(251, 251, 251)",
+                          borderRadius: "20px",
+                          height: "40px",
+                          marginLeft: "10px",
+                          width: "40px",
+                          cursor: "pointer",
+                          fontSize: "18px",
+                          marginBottom: "15px",
+                          border: "none",
+                          textDecoration: "none",
+                          backgroundColor: " rgba(28, 27, 27, 0.48)"
+                        }}>
+                          <IoMdShare />
+                        </button></a>
+                      </div>
+
+
+                    </div>}
 
                 </div>
-                
 
-                  {/* {
+
+              </div>
+
+
+              {/* {
                     <div className='imgdescription'>
                       <p  style={{cursor:"pointer"}} onClick={() => setShowMore(!showMore)}> {showMore ? items[currentIndex].description 
                   : items[currentIndex].description.substring(0, maxLength) }
@@ -247,15 +252,15 @@ const NewsFeed = () => {
                
                           
                     </div> } */}
-               
-              </>
-            )
-          }
 
-        </div>
-       
-            </div>
-   
+            </>
+          )
+        }
+
+      </div>
+
+    </div>
+
   )
 }
 
